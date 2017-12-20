8210287f894b506ee29ee4d37bec50407658c0c8
achroscielewska <achroscielewska@gmail.com>
2017-12-20 12:20:28 +0100

    inicjalizacja aplikacji react

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return <h1>Outfits by weather</h1>
+}
+ReactDOM.render(
+<App/>,
+    document.getElementById('root')
+)
