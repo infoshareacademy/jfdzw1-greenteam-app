e55d913c3fa6663202553ff9f7ff042f7c9cb8d4
wojtexnaj <wojciech.najda.sg@gmail.com>
2018-01-21 11:45:43 +0100

    Created CurrentWeather component

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -1,17 +1,14 @@
 import React from 'react';
 
 class CurrentWeather extends  React.Component {
-
   constructor(props) {
     super(props);
   }
+
   render() {
     return(
       <div>Hello!</div>
     );
   }
-
 }
-
-
 export default CurrentWeather;
