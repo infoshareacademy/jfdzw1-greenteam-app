5f29ba0ce6eb5d61a16ef95cfcc240e94606f6d0
wojtexnaj <wojciech.najda.sg@gmail.com>
2018-01-21 13:37:54 +0100

    Created favorite choices component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import React from 'react'
 import { Jumbotron} from 'react-bootstrap'
 import ListOfClothesItems from './components/ListOfClothesItems'
 import SignUp from './components/SignUp'
+import FavoriteChoices from "./components/FavoriteChoices";
 const App = () => [
     <Jumbotron>
         <h1>Outfits by weather</h1>
         <SignUp/>
     </Jumbotron>,
-
     <ListOfClothesItems/>
 
     ]
---
+++ src/components/FavoriteChoices.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class FavoriteChoices extends React.Component {
+  constructor (props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div>Favorite choices.</div>
+    );
+  }
+}
+export default FavoriteChoices;
