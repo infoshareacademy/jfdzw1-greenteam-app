cd4babb82f319e271436a6784071780ae52c953a
wojtexnaj <wojciech.najda.sg@gmail.com>
2018-01-21 11:29:18 +0100

    Created CurrentWeather component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import React from 'react'
 import { Jumbotron} from 'react-bootstrap'
 import ListOfClothesItems from './components/ListOfClothesItems'
 import SignUp from './components/SignUp'
+import CurrentWeather from './components/CurrentWeather';
+
 const App = () => [
     <Jumbotron>
         <h1>Outfits by weather</h1>
         <SignUp/>
     </Jumbotron>,
 
-    <ListOfClothesItems/>
+    <ListOfClothesItems/>,
 
     ]
 
---
+++ src/components/CurrentWeather.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class CurrentWeather extends  React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return(
+      <div>Hello!</div>
+    );
+  }
+
+}
+
+
+export default CurrentWeather;
