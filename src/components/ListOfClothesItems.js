4b2725b7720241ff04635a791499939a6c707a9c
achroscielewska <achroscielewska@gmail.com>
2018-01-22 17:33:28 +0100

    JFDZW1TG-75 | dodany plik dataset.json

--- src/components/ListOfClothesItems.js
+++ src/components/ListOfClothesItems.js
@@ -6,29 +6,11 @@ import dataset from '../dataset/dataset.json';
 import ClothItem from '../components/ClothItem'
 
 class ListOfClothesItems extends Component {
-    state = {
-        user: {}
-    };
-
-    handleChange = (e) => {
-        e.preventDefault();
-        console.log(e)
-    }
 
     render() {
 
         return (
             <div>
-                <Col>
-                    <Form>
-                        <FormControl componentClass="select" placeholder="select">
-                            <option value="all">all</option>
-                            <option value="female">female</option>
-                            <option value="male">male</option>
-                        </FormControl>
-                    </Form>
-                </Col>
-
                 {dataset
                     .filter(item =>item.gender === 'female')
                     .map((item,i) =>
