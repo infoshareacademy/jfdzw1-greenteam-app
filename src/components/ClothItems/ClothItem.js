cd0686f5c6e43dc3bcd848dc27e0390fa06087ff
achroscielewska <achroscielewska@gmail.com>
2018-02-20 21:57:41 +0100

    inspirations | style pod przyciskiem add i added to favorites

--- src/components/ClothItems/ClothItem.js
+++ src/components/ClothItems/ClothItem.js
@@ -7,13 +7,19 @@ const ClothItem = (props) => {
     const item = {
         id: props.id,
         img: props.img,
+        favorite: props.favorite
     };
 
     return (
         <Col xs={12} sm={6} md={3}>
             <div className ='colthItem'>
                 <Thumbnail href="#" alt="171x180" src={item.img} />
-                <button className ='addToFavorites' >Add to favorites</button>
+
+                {item.favorite ?
+                    <button className ='addedToFavorites'>Added to favorites</button>
+                    : <button className ='addToFavorites'>Add to favorites</button>}
+
+
             </div>
         </Col>
     )
